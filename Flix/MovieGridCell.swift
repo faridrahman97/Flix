21d6c683cdf63341c9b8f5956cb43b8c951cce9e
Farid Rahman <beatsbyfar.id@gmail.com>
2019-04-23 19:32:55 -0700

    Finished working on part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Farid Rahman on 4/22/19.
+//  Copyright © 2019 Farid Rahman. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
